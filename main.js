23110ca0699b91659b5d80b0fb43e531cb95dcdd
VSDevloper <84179803+VSDevloper@users.noreply.github.com>
2021-11-06 13:22:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,8 +13,24 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 480, 380);
+    if(status !="");
+    {
+        objectDetector.detect(video, gotResult);
+        for(I = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : objects Dectected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detedted are : "+ object.length;
+
+fill("#FF0000");
+percent = floor(objects[i].confidence * 100);
+text(objects[i].label + "" + percent + "%" , objects[i].x + 15, objetcs[i].y +15);
+nofill();
+storke("#FF0000");
+rect(objetcs[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+    }
 }
-function start()
+function status ()
 {
     objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
     document.getElementById("status").innerHTML = "Status : Dectecting Objects";
@@ -26,4 +42,13 @@ function modelLoaded()
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+function gotResult(error, results);
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
